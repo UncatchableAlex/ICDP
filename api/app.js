e13105cc74a4755c8812d4edf5e81566141728f2
Alex Meislich <alex.meislich@gmail.com>
2022-12-01 02:19:07 -0800

    Express middleware now can verify passwords from frontend against RDS
    I still need to implement the case of the user not existing. The frontend needs a redirect in the case of a successful login and suitable errors in the case of unsuccessful attempts. The dummy accounts that I have made are: DjAlex-password1, CalamityClark-password1, Tmoney-password1, BigBodine-pword. I will give the super secret json file with db credentials upon request so you cif anyone wants to play around with it

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 const testRouter = require("./routes/test");
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 app.use("/test", testRouter);
 
 // catch 404 and forward to error handler
